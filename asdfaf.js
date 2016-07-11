ae5db24e9aec9a6a0797cee16f90e92e00e74482
chris <asdfasd@afasdf.com>
2016-07-11 10:09:27 -0700

    removing old js file

--- asdfaf.js
+++
@@ -1,9 +0,0 @@
-function factorialize(num) {
-    var total = 1;
-    for (var i = 1; i < num + 1; i += 1) {
-        total = (total * i);
-    }
-    return total
-}
-
-console.log(factorialize(5));
